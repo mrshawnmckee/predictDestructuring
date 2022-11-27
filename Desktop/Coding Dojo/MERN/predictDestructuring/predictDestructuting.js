a5de509faaa54557f4d170ecef06f50459907471
mrshawnmckee <116242881+mrshawnmckee@users.noreply.github.com>
2022-11-27 13:11:34 -0500

    first commit

---
+++ Desktop/Coding Dojo/MERN/predictDestructuring/predictDestructuting.js
@@ -0,0 +1,66 @@
+//Problem 1 //
+
+const cars = ['Tesla', 'Mercedes', 'Honda']
+const [ randomCar ] = cars
+const [ ,otherRandomCar ] = cars
+//Predict the output
+console.log(randomCar)
+console.log(otherRandomCar)
+// This will console log 'Tesla', 'Mercedes' //
+
+// Problem 2 //
+
+const employee = {
+    name: 'Elon',
+    age: 47,
+    company: 'Tesla'
+}
+const { name: otherName } = employee;
+//Predict the output
+console.log(name);
+console.log(otherName);
+//Will only print the second 'Elon' because name is not in scope //
+
+// Problem 3 //
+
+const person = {
+    name: 'Phil Smith',
+    age: 47,
+    height: '6 feet'
+}
+const password = '12345';
+const { password: hashedPassword } = person;  
+//Predict the output
+console.log(password);
+console.log(hashedPassword);
+//console will log 12345, there is no password in person to be assigned to the hashed password variable //
+
+//Problem 4 //
+
+const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+const [,first] = numbers;
+const [,,,second] = numbers;
+const [,,,,,,,,third] = numbers;
+//Predict the output
+console.log(first == second);
+console.log(first == third);
+// console will log false, true //
+
+//Problem 5 //
+
+const lastTest = {
+    key: 'value',
+    secondKey: [1, 5, 1, 8, 3, 3]
+}
+const { key } = lastTest;
+const { secondKey } = lastTest;
+const [ ,willThisWork] = secondKey;
+//Predict the output
+console.log(key);
+console.log(secondKey);
+console.log(secondKey[0]);
+console.log(willThisWork);
+// the console will log:value, [1,2,1,8,3,3], 1, 5 //
+
+
+
